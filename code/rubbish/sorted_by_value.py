e124e19bcb232f018f25cb073437d711b4810ccb
enricorubagotti <66751509+enricorubagotti@users.noreply.github.com>
2023-11-07 15:07:45 -0500

    Delete code directory

--- code/rubbish/sorted_by_value.py
+++
@@ -1,8 +0,0 @@
-my_dict = {"apple": 5, "banana": 3, "cherry": 8, "date": 1}
-
-# Sort the dictionary items by their values in descending order
-sorted_items = sorted(my_dict.items(), key=lambda x: x[1], reverse=True)
-
-# Iterate over the sorted keys
-for key, value in sorted_items:
-    print(str(key)+"="+str(value))
